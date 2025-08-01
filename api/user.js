16287e141c1889b1d405bb5daee597246a00fda5
wicktg <thematrix7417@gmail.com>
2025-08-01 15:37:43 +0530

    New version

--- api/user.js
+++ api/user.js
@@ -6,12 +6,18 @@ export default async function handler(req, res) {
   }
 
   try {
-    const apiRes = await fetch(
-      `https://gomtu.xyz/api/yap/open?username=${username}`
+    const res30 = await fetch(
+      "https://kaito.irys.xyz/api/community-mindshare?window=30d"
     );
-    const data = await apiRes.json();
+    const data30 = await res30.json();
+
+    const res90 = await fetch(
+      "https://kaito.irys.xyz/api/community-mindshare?window=90d"
+    );
+    const data90 = await res90.json();
+
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
+    res.status(200).json({ data30, data90 });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
